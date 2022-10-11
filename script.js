c87a0398ff831fb289f6ad290e656b5b3e83d61b
MarieMakesMoneys <marie.collecchia@gmail.com>
2022-10-11 13:57:23 +0200

    Remake of functions & associated styles Make the program run with a prompt and a variable number of cells

--- script.js
+++ script.js
@@ -2,21 +2,36 @@ const canvas = document.querySelector(".canvas");
 
 const gridSide = Number(prompt("How many cells in a row ?", "16"));
 
-function numOfGridItems(gridSide) {
-    let numberOfGridItems = gridSide * gridSide;
-    return numberOfGridItems; 
-};
-
-function createGridItem(gridSide) {
-    for (let i = 1; i <= numOfGridItems(gridSide); i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        canvas.appendChild(gridItem);
+function createCell(gridSide) {
+    if (gridSide > 100) {
+        gridSide = 100;
+        for (let i = 1; i <= gridSide; i++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            for (let i = 1; i <= gridSide; i++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                column.appendChild(cell);
+            }
+            canvas.appendChild(column);
+        }
+    } else {
+        for (let i = 1; i <= gridSide; i++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            for (let i = 1; i <= gridSide; i++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                column.appendChild(cell);
+            }
+            canvas.appendChild(column);
+        }
     }
 }
-createGridItem(gridSide);
+createCell(gridSide);
 
-const cells = document.querySelectorAll(".grid-item");
+
+const cells = document.querySelectorAll(".cell");
 
 cells.forEach((cell) => {
     cell.addEventListener("dragover", (e) => {
